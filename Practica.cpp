3e08e931998ad7d0e7c36c3ad3be08afabfafce8
Usuario <Usuario@DESKTOP-2631UIQ>
2024-02-27 15:35:19 -0400

    Agregar archivos de proyecto.

---
+++ Practica.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+
+class Animal {
+private:
+    std::string nombre;
+
+public:
+    Animal(std::string n) : nombre(n) {}
+
+    virtual void emitirSonido() const = 0;
+
+    std::string getNombre() const {
+        return nombre;
+    }
+};
+
+
+class Gato : public Animal {
+public:
+    Gato(std::string n) : Animal(n) {}
+
+    void emitirSonido() const override {
+        std::cout << "Miau" << std::endl;
+    }
+};
+
+class Perro : public Animal {
+public:
+    Perro(std::string n) : Animal(n) {}
+
+    void emitirSonido() const override {
+        std::cout << "Guau" << std::endl;
+    }
+};
+
+
+void hacerSonar(const Animal* animal) {
+    animal->emitirSonido();
+}
+
+
+int main() {
+    Gato gato("Nermal");
+    Perro perro("Odi");
+
+    std::cout << gato.getNombre() << " hace: ";
+    hacerSonar(&gato);
+
+    std::cout << perro.getNombre() << " hace: ";
+    hacerSonar(&perro);
+
+    return 0;
+}
